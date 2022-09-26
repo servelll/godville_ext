ae6f8c5e9f3d35ef98a38ec102585259891b0704
DamGer <damir.gerasemenyuk@bk.ru>
2022-09-26 17:59:58 +0300

    mini quests filling

--- logic/shared.js
+++ logic/shared.js
@@ -2,6 +2,7 @@
 //test
 // test22
 //test33
+
 String.prototype.toHtmlEntities = function () {
     return this.replace(/./gm, function (s) {
         // return "&#" + s.charCodeAt(0) + ";";
@@ -79,4 +80,98 @@ function fillPushesAtThisSubstep(index, subindex, submove_owner_letter) {
     }
 
     //далее, итерация только по submove_owner_letter, subindex и d[subindex] не используем
+}
+
+
+/*function getArrayViaFrom(dim1, dim2) {
+    //console.time('Execution Time');
+    let output = Array.from(Array(dim1), () => Array[dim2]);
+    //console.timeEnd('Execution Time');
+    return output;
+  }*/
+
+function getArrayViaFor(dim1, dim2) {
+    //console.time('Execution Time 1');
+    let output = [];
+    for (let i = 1; i <= dim1; i++) {
+        output.push(Array(dim2));
+    }
+    //console.timeEnd('Execution Time 1');
+    return output;
+}
+
+/*function createArray(length) {
+    let arr = new Array(length || 0),
+        i = length;
+    if (arguments.length > 1) {
+        let args = Array.prototype.slice.call(arguments, 1);
+        while(i--) arr[length-1 - i] = createArray.apply(this, args);
+    }
+    return arr;
+}
+
+  console.time('Execution Time 1');
+  console.log(getArrayViaFrom(100)) // Takes 10x more than for that is 0.220ms
+  console.timeEnd('Execution Time 1')
+  console.time('Execution Time 1');
+  console.log(getArrayViaFor(100))  // Takes 10x less than From that is 0.020ms
+  console.timeEnd('Execution Time 1')
+  console.time('Execution Time 1');
+  console.log(createArray(100, 1))
+  console.timeEnd('Execution Time 1'); */
+
+let miniQuestsObj = {
+    newquests: {
+        recency: 'новый мини-квест',
+        quests: []
+    },
+    oldquests: {
+        recency: 'старый мини-квест',
+        quests: []
+    }
+}
+
+async function getPageFromUrl(url) {
+	let response = await fetch(url);
+	const html_text = await response.text();
+	const parser = new DOMParser();
+	const html = parser.parseFromString(html_text, "text/html");
+	return html;
+}
+
+async function parseMiniQuestsTitles(link) {
+    let html      = await getPageFromUrl(link);
+    let raw_data  = html.querySelector("#post-body-1560386").textContent;
+    let data      = raw_data.slice(raw_data.indexOf('Старые мини-квесты') + 18).replace("\nНовые мини-квесты", ' * Metka * ').replace('\n', '').trim();
+    let rawMass   = data.split(' * ');
+    let oldQuests = rawMass.slice(0, rawMass.indexOf('Metka'));
+    let newQuests = rawMass.slice(rawMass.indexOf('Metka') + 1);
+    return { oldQuests, newQuests: newQuests };
+}
+
+function fillMiniQuestsBlanks(fromArr, toArr, regy) {
+    fromArr.forEach(function (el, index) {
+        toArr[index] = { blank: [] };
+        if (el.includes('(в этом мини-квесте часто этапы меняются местами)')) {
+            toArr[index].warning = '(в этом мини-квесте часто этапы меняются местами)';
+        }
+        el.match(regy).forEach(function (item) {
+            toArr[index]['blank'].push(item.trim());
+        });
+    });
+}
+
+function fillMiniQuestsTitles() {
+    parseMiniQuestsTitles("https://godville.net/forums/show_topic/2460?page=254#post_1560386").then(mini_quests => {
+        let regy = /([А-ЯЁ0-9][^\→]+)(?=(\→|$))/gi;
+        //let godvilleMiniQuests = getArrayViaFor(mini_quests['oldQuests'].length + mini_quests['newQuests'].length, 0);
+        //let rawMass = mini_quests.oldQuests.concat(mini_quests.newQuests);
+        fillMiniQuestsBlanks(mini_quests.oldQuests, miniQuestsObj['oldquests']['quests'], regy);
+        fillMiniQuestsBlanks(mini_quests.newQuests, miniQuestsObj['newquests']['quests'], regy);
+        //console.log(JSON.stringify(miniQuestsObj));
+        //localStorage.setItem('godvilleMiniQuests', JSON.stringify(godvilleMiniQuests));
+        localStorage.setItem('AutoGV_miniQuests', JSON.stringify(miniQuestsObj));
+        let data = new Date();
+        //"mini_quests_time"
+    });
 }
